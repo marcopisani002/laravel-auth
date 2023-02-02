56ecc4dbe711db02d97bae5fd420e6899e015ebb
marcopisani002 <pisanimarco34@gmail.com>
2023-02-02 10:42:28 +0100

    add animations

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <div class=" p-5 mt-5 bg-dark text-white ">
-    <div class="container py-5">
+    <div class="container py-5 anim">
        
         <h3>Hola! nice to meet you</h3>
         <h1 class="display-5 fw-bold">
